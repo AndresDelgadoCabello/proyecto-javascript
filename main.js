018353e6b7b9bdf45a1070243ad247a8857e83ed
AndresDelgadoCabello <andresdelgadocabello99@gmail.com>
2022-11-19 14:07:09 -0300

    reentrega

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
-const input = document.getElementById("input");
-const imagenes = document.getElementsByClassName("main__imagenes")
-[0];
+const altura = document.querySelector("#altura")
+const peso = document.querySelector("#peso")
+const resultado = document.querySelector("#resultado")
 
-input.addEventListener("keydown", function(event){
-    if(event.key === "enter")
-    loadImg();
-})
 
-function loadImg(){
-    removerImg();
+const calcularIMC = () => {
 
-        const url = "https://api.unsplash.com/search/photos/?query="+input.value+"&per_page=5&client_id=9FvYye812LCVi5q7wrXD7dF_dUea4Wl5MLpuivvMREs";
-        fetch(url)
-        .then(response =>{
-        if(response.ok)
-        return response.json();
-        else
-        alert(response.status)
-})
+if (altura.value !== '' && peso.value !== '') {
+    const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
+    let clasificacion = ""
 
-.then(data =>{
-    const imageNodes = [];
-    for(let i = 0; i < data.results.length;i++){
-        imageNodes[i] = document.createElement("div");
-        imageNodes[i].className = "img";
-        imageNodes[i].style.backgroundImage = "url("+data.results[i].urls.raw+")";
-        imageNodes[i].addEventListener("dblclick", function(){
-            window.open(data.results[i].links.download, "_blank");
-        })
-        imagenes.appendChild(imageNodes[i]);
+    if (imc < 18.5) {
+    clasificacion = "Abajo del peso ideal"
+    } else if (imc < 25) {
+    clasificacion = "Felicidades! te encuentras en tu Peso ideal"
+    } else if (imc < 30) {
+    clasificacion = "Sobrepeso"
+    } else if (imc < 35) {
+    clasificacion = "obesidad grado 1"
+    } else if (imc < 41) {
+    clasificacion = "Obesidad grado 2"
+    } else {
+    clasificacion = "Obesidad grado 3"
     }
-})
-
+    
+    resultado.innerHTML = `IMC: ${imc} (${clasificacion})`
+} else {
+    resultado.innerHTML = "resultados"
 }
-function removerImg(){
-    imagenes.innerHTML = "";
 }
